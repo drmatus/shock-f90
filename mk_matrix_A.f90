0ee1aa2c17b48dc245ba87da2d0624644dd5eb03
drmatus <drmatus.42@gmail.com>
2021-06-30 19:26:23 -0400

    Modificado para poder usar tridag.f90

--- mk_matrix_A.f90
+++ mk_matrix_A.f90
@@ -1,19 +1,18 @@
-subroutine mk_matrix_A(A, v1, n,  alpha, theta)
+subroutine mk_matrix_A(Aa, Ab, Ac, v1, n,  alpha, theta)
     implicit none
-    real, dimension(n,n) :: A
+    real, dimension(n) :: Aa, Ab, Ac
     real, dimension(n)   :: v1
     real                 :: alpha, theta
     integer              :: n, i
 
-    A(1,1)   = 1
-    A(1,2)   =  theta*alpha*v1(2)
-    A(n,n)   = 1
-    A(n,n-1) = -theta*alpha*v1(n-1)
-
-    do i=2, n-1
-        A(i, i-1) = -theta * alpha * v1(i-1)
-        A(i,i)    =  1
-        A(i, i+1) =  theta * alpha * v1(i+1)
+    Ab(1) = 1.0
+    Ac(1) = theta*alpha*v1(2)
+    Aa(n) = -theta*alpha*v1(n-1)
+    Ab(n) = 1.0
+    do i=2,n-1
+        Aa(i) = -theta*alpha*v1(i-1)
+        Ab(i) = 1.0
+        Ac(i) = theta*alpha*v1(i+1)
     end do
 
     return
