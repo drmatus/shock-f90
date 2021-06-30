4f6f96bf91504f646455efb2f9eb108cc05d88c0
drmatus <drmatus.42@gmail.com>
2021-06-30 19:26:57 -0400

    Reemplazo division erronea

--- timestep.f90
+++ timestep.f90
@@ -6,12 +6,17 @@ SUBROUTINE timestep(c,v,n, eps, dx, dt)
     REAL :: dt, dt_1, maxv
 
     dt_1 = 1.0/max(abs(c(1)), abs(v(1)))
+!   dt_1 = 1.0/(abs(c(1))+ abs(v(1)))
     do i=2,n
+!       maxv = 1.0/(abs(c(i))+ abs(v(i)))
         maxv = 1.0/max(abs(c(i)), abs(v(i)))
         dt_1 = min(maxv, dt_1)
     END do
 
-    dt = eps*dx/dt_1
+    dt = eps*dx*dt_1
+    if (dt < 100.0) then
+        dt = 100.0
+    end if
 
     RETURN
     end
